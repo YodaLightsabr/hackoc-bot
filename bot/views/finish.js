a2d9b360373ff5410e3e27d2f3ebeac0485f22e2
Yoda <yoda@yodacode.xyz>
2022-09-21 23:38:44 -0700

    short term fix

--- bot/views/finish.js
+++ bot/views/finish.js
@@ -17,18 +17,11 @@ export default function finish ({ userID }) {
             <content>Welcome to {'<:hackoc:1022357235575308390>'} Hack OC, {`<@${userID}>`}! Have you taken a look at our {'<#1018263922299645994>'} yet?</content>
             <ActionRow>
                 <StatelessButton onClick={click => {
+                    click.member.roles.add('1018263397571231755');
+                    click.guild.channels.cache.get('1022350908333490226').send(`Welcome, <@${click.user.id}>. Why don't you introduce yourself?`);
                     click.view(
                         <message>
-                            <content>Awesome! Why don't we have a look at the rest of the server now?</content>
-                            <components_arr> {/** output an array of Discord message components */}
-                                <component type={1}>
-                                    <StatelessButton onClick={click => {
-                                        click.member.roles.add('1018263397571231755');
-                                        click.guild.channels.cache.get('1022350908333490226').send(`Welcome, <@${click.user.id}>. Why don't you introduce yourself?`);
-                                        click.channel.delete();
-                                    }} label="Let's go!" variant={3} />
-                                </component>
-                            </components_arr>
+                            <content>Awesome! Why don't we have a look at the rest of the server now? Check out {'<#1022350908333490226>'}.</content>
                         </message>
                     )
                 }} label="Yep!" variant={3} />
