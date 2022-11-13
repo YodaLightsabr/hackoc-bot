24c6d984cce9c41c50322911b9724e26e7704c8d
Yoda <yoda@yodacode.xyz>
2022-11-12 18:18:01 -0800

    asdgdf

--- bot/views/welcome.js
+++ bot/views/welcome.js
@@ -16,7 +16,7 @@ export default function () {
         <message content="Welcome to Conflict!">
             <embeds>
                 <embed color="#ff3333" image={{ url: "https://www.gitbook.com/cdn-cgi/image/width=32,height=32,fit=contain,dpr=1,format=auto/https%3A%2F%2Ffiles.gitbook.com%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FqKkjNKHtBhCUHveEGTY2%252Ficon%252F2sgixS2pVqtWMiBvRGg2%252FConflict.png%3Falt%3Dmedia%26token%3D24c0b0f6-f987-46ca-b4a5-8f193d650bcb" }}>
-                    <title>Conflict</title>
+                    <title>Conflict 2</title>
                     <description>
                         Build better **{phrases[Math.floor((Math.random() * 1000)) % 6]}**.
                     </description>
