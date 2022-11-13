4496d198a5f3bd459ff45cee6935a6d54f1cd61a
Yoda <yoda@yodacode.xyz>
2022-11-12 18:19:15 -0800

    sdgfsdgd

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,5 +1,5 @@
 import Command from '@conflict/beta/commands';
-import { Embed } from '@conflict/beta/components';
+import { Embed, ActionRow, Button } from '@conflict/beta/components';
 
 export default new Command({
     name: 'help',
@@ -15,6 +15,15 @@ export default new Command({
                     <title>Hello, world!</title>
                     <description>Welcome to **Conflict**.</description>
                 </Embed>
+                <ActionRow>
+                    <Button onClick={click => {
+                        click.view(
+                            <message>
+                                <content>Test</content>
+                            </message>
+                        )
+                    }} label="Test" variant={2} />
+                </ActionRow>
             </message>
         );
     }
--- bot/views/welcome.js
+++ bot/views/welcome.js
@@ -16,21 +16,12 @@ export default function () {
         <message content="Welcome to Conflict!">
             <embeds>
                 <embed color="#ff3333" image={{ url: "https://www.gitbook.com/cdn-cgi/image/width=32,height=32,fit=contain,dpr=1,format=auto/https%3A%2F%2Ffiles.gitbook.com%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FqKkjNKHtBhCUHveEGTY2%252Ficon%252F2sgixS2pVqtWMiBvRGg2%252FConflict.png%3Falt%3Dmedia%26token%3D24c0b0f6-f987-46ca-b4a5-8f193d650bcb" }}>
-                    <title>Conflict 2</title>
+                    <title>Conflict</title>
                     <description>
                         Build better **{phrases[Math.floor((Math.random() * 1000)) % 6]}**.
                     </description>
                 </embed>
             </embeds>
-            <ActionRow>
-                <Button onClick={click => {
-                    click.view(
-                        <message>
-                            <content>Test</content>
-                        </message>
-                    )
-                }} label="Test" variant={2} />
-            </ActionRow>
         </message>
     ).useHooks(deleteAfter(5000));
 }
