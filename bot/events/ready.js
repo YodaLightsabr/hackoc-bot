d54c418f3af8fa8daabef357c540df7eaa09b2a0
Yoda <yoda@yodacode.xyz>
2022-11-12 18:16:56 -0800

    aa

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -2,6 +2,6 @@ import { onReady } from "@conflict/beta/events";
 import { stump } from '@conflict/beta/logger';
 
 onReady(client => {
-    client.channels.cache.get('1041173665770909747').send('Ready');
     stump.info("Ready")
+    client.channels.cache.get('1041173665770909747').send('Ready');
 })
